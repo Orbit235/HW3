a92f504073eede7aa05c89e03feb476f3d854cd8
unknown <v.ionichev@gmail.com>
2022-11-20 13:55:44 +0300

    HW4

--- js/main.js
+++ js/main.js
@@ -3,4 +3,27 @@ $(() => {
         $('.hbl').toggleClass('d-sm-none')
         // $('.hbl').toggleClass('closed')
     })
+    $('#form>button').click((e) => {
+        e.preventDefault()
+
+        let data = $('#form').serializeArray()
+
+        if (Number(data[0].value) > 4 || Number(data[0].value) < -4) {
+            $('.x-error').removeClass("d-none")
+        } else {
+            $('.x-error').addClass("d-none")
+        }
+        if (!data[2]) {
+            console.log('нету R')
+            $('.r-error').removeClass("d-none")
+
+        } else {
+            $('.r-error').addClass("d-none")
+
+        }
+
+        console.log(data)
+        let x = $('#x')
+        console.log(x);
+    })
 })
